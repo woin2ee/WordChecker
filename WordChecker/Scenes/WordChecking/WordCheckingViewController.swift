7f86304375aca078719f05fc2e87e41ef939424f
Jaewon Yun <woin2ee@gmail.com>
2023-08-28 23:44:30 +0900

    Feat: Pull down menu button 으로 변경 / 현재 단어 삭제 기능 추가

--- WordChecker/Scenes/WordChecking/WordCheckingViewController.swift
+++ WordChecker/Scenes/WordChecking/WordCheckingViewController.swift
@@ -24,7 +24,13 @@ final class WordCheckingViewController: UIViewController {
         return label
     }()
     
-    let nextButton: BottomButton = .init(title: WCString.next)
+    lazy var nextButton: BottomButton = {
+        let button: BottomButton = .init(title: WCString.next)
+        button.addAction(.init { [weak self] _ in
+            self?.viewModel.updateToNextWord()
+        }, for: .touchUpInside)
+        return button
+    }()
     
     lazy var listButton: BottomButton = {
         let button: BottomButton = .init(title: WCString.list)
@@ -36,8 +42,6 @@ final class WordCheckingViewController: UIViewController {
         button.accessibilityIdentifier = AccessibilityIdentifier.WordChecking.listButton
         return button
     }()
-    
-    let shuffleButton: BottomButton = .init(title: WCString.shuffleOrder)
     
     lazy var translateButton: BottomButton = {
         let button: BottomButton = .init(title: WCString.translate)
@@ -59,11 +63,7 @@ final class WordCheckingViewController: UIViewController {
         return button
     }()
     
-    let addWordButton: UIBarButtonItem = {
-        let button: UIBarButtonItem = .init(systemItem: .add)
-        button.accessibilityIdentifier = AccessibilityIdentifier.WordChecking.addWordButton
-        return button
-    }()
+    let pullDownButton: UIBarButtonItem = .init(image: .init(systemName: "ellipsis.circle"))
     
     init(viewModel: WordCheckingViewModel) {
         self.viewModel = viewModel
@@ -91,7 +91,6 @@ final class WordCheckingViewController: UIViewController {
         self.view.addSubview(wordLabel)
         self.view.addSubview(listButton)
         self.view.addSubview(nextButton)
-        self.view.addSubview(shuffleButton)
         self.view.addSubview(translateButton)
         
         NSLayoutConstraint.activate([
@@ -114,23 +113,54 @@ final class WordCheckingViewController: UIViewController {
             nextButton.widthAnchor.constraint(equalTo: listButton.widthAnchor),
             self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: nextButton.bottomAnchor, multiplier: 1.0),
         ])
-        // shuffle button
-        NSLayoutConstraint.activate([
-            shuffleButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: buttonCollectionSpacingToSafeArea),
-            shuffleButton.widthAnchor.constraint(equalTo: listButton.widthAnchor),
-            listButton.topAnchor.constraint(equalToSystemSpacingBelow: shuffleButton.bottomAnchor, multiplier: 1.0),
-        ])
         // translate button
         NSLayoutConstraint.activate([
-            translateButton.leadingAnchor.constraint(equalToSystemSpacingAfter: shuffleButton.trailingAnchor, multiplier: 1.0),
-            translateButton.widthAnchor.constraint(equalTo: shuffleButton.widthAnchor),
+            translateButton.widthAnchor.constraint(equalTo: nextButton.widthAnchor),
             self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: translateButton.trailingAnchor, constant: buttonCollectionSpacingToSafeArea),
             nextButton.topAnchor.constraint(equalToSystemSpacingBelow: translateButton.bottomAnchor, multiplier: 1.0),
         ])
     }
     
     private func setupNavigationBar() {
-        self.navigationItem.rightBarButtonItem = addWordButton
+        self.navigationItem.rightBarButtonItem = pullDownButton
+        let menu: UIMenu = .init(children: [
+            UIAction(title: WCString.addWord, image: .init(systemName: "plus.app"), handler: { [weak self] _ in
+                let alertController = UIAlertController(title: WCString.addWord, message: "", preferredStyle: .alert)
+                let cancelAction: UIAlertAction = .init(title: WCString.cancel, style: .cancel)
+                let addAction: UIAlertAction = .init(title: WCString.add, style: .default) { [weak self] _ in
+                    guard let word = alertController.textFields?.first?.text else {
+                        return
+                    }
+                    self?.viewModel.saveNewWord(word)
+                }
+                alertController.addAction(cancelAction)
+                alertController.addAction(addAction)
+                alertController.addTextField { textField in
+                    let action: UIAction = .init { _ in
+                        let text = textField.text ?? ""
+                        if text.isEmpty {
+                            addAction.isEnabled = false
+                        } else {
+                            addAction.isEnabled = true
+                        }
+                    }
+                    textField.addAction(action, for: .allEditingEvents)
+                }
+                self?.present(alertController, animated: true)
+            }),
+            UIAction(title: WCString.shuffleOrder, image: .init(systemName: "shuffle"), handler: { [weak self] _ in
+                self?.viewModel.shuffleWordList()
+            }),
+            UIAction(
+                title: WCString.deleteWord,
+                image: .init(systemName: "trash", withConfiguration: UIImage.SymbolConfiguration.init(hierarchicalColor: .systemRed)),
+                attributes: .destructive,
+                handler: { [weak self] _ in
+                    self?.viewModel.deleteCurrentWord()
+                }
+            ),
+        ])
+        pullDownButton.menu = menu
     }
     
     private func bindViewModel() {
@@ -143,39 +173,6 @@ final class WordCheckingViewController: UIViewController {
                 }
             }
             .store(in: &cancellableBag)
-        
-        shuffleButton.addAction(.init { [weak self] _ in
-            self?.viewModel.shuffleWordList()
-        }, for: .touchUpInside)
-        
-        nextButton.addAction(.init { [weak self] _ in
-            self?.viewModel.updateToNextWord()
-        }, for: .touchUpInside)
-        
-        addWordButton.primaryAction = .init { [weak self] _ in
-            let alertController = UIAlertController(title: WCString.addWord, message: "", preferredStyle: .alert)
-            let cancelAction: UIAlertAction = .init(title: WCString.cancel, style: .cancel)
-            let addAction: UIAlertAction = .init(title: WCString.add, style: .default) { [weak self] _ in
-                guard let word = alertController.textFields?.first?.text else {
-                    return
-                }
-                self?.viewModel.saveNewWord(word)
-            }
-            alertController.addAction(cancelAction)
-            alertController.addAction(addAction)
-            alertController.addTextField { textField in
-                let action: UIAction = .init { _ in
-                    let text = textField.text ?? ""
-                    if text.isEmpty {
-                        addAction.isEnabled = false
-                    } else {
-                        addAction.isEnabled = true
-                    }
-                }
-                textField.addAction(action, for: .allEditingEvents)
-            }
-            self?.present(alertController, animated: true)
-        }
     }
     
 }
