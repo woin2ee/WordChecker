37de466bd1f20d4b40ac7380a0cb04a8742cc7a2
Jaewon Yun <woin2ee@gmail.com>
2023-09-01 18:27:07 +0900

    빌드 시 `$swiftlint --fix` 명령어가 실행되지 않도록 변경

--- Scripts/run_swiftlint.sh
+++ Scripts/run_swiftlint.sh
@@ -5,7 +5,7 @@ if [[ "$(uname -m)" == arm64 ]]; then
 fi
 
 if which swiftlint > /dev/null; then
-    swiftlint --fix && swiftlint
+    swiftlint
 else
     echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
