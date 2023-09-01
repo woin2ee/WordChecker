fa9d0a202dc8a4ef54cba01cf44f8a5e0cc872f8
Jaewon Yun <woin2ee@gmail.com>
2023-09-01 16:09:31 +0900

    Add script to set githooks path

---
+++ Scripts/set_githooks_path.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CURRENT_HOOKS_PATH=$(git config --get core.hooksPath)
+PROJECT_HOOKS_PATH=".githooks"
+
+if [ ${CURRENT_HOOKS_PATH} != ${PROJECT_HOOKS_PATH} ]; then
+    git config core.hooksPath ${PROJECT_HOOKS_PATH}
+fi
