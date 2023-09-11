d8f196b533e079ce504070f3938227e9bc1c5a8f
Jaewon Yun <woin2ee@gmail.com>
2023-09-11 21:47:58 +0900

    Change WordCheckingView UI

---
+++ Sources/WordCheckerShared/Scenes/WordChecking/Views/WordChecking+ChangeSymbol.swift
@@ -0,0 +1,44 @@
+//
+//  WordChecking+ChangeWordSymbol.swift
+//  WordChecker
+//
+//  Created by Jaewon Yun on 2023/09/11.
+//
+
+import SFSafeSymbols
+import UIKit
+
+extension WordCheckingViewController {
+
+    final class ChangeWordSymbol: UIImageView {
+
+        init(direction: Direction) {
+            super.init(frame: .zero)
+
+            let symbolScale: UIImage.SymbolScale = .large
+            let config: UIImage.SymbolConfiguration = .init(scale: symbolScale)
+
+            switch direction {
+            case .left:
+                self.image = .init(systemSymbol: .chevronCompactLeft, withConfiguration: config)
+            case .right:
+                self.image = .init(systemSymbol: .chevronCompactRight, withConfiguration: config)
+            }
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        // swiftlint:disable:next nesting
+        enum Direction {
+
+            case left
+
+            case right
+
+        }
+
+    }
+
+}
