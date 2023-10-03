b5e036ef0dd426fb2c9eeac5c33f5b021844a363
Jaewon Yun <woin2ee@gmail.com>
2023-10-03 19:20:34 +0900

    Simplify View-ViewModel binding

---
+++ Sources/iOSCore/Scenes/UserSettings/UserSettingsItems/UserSettingsSection.swift
@@ -0,0 +1,19 @@
+//
+//  UserSettingsSection.swift
+//  iOSCore
+//
+//  Created by Jaewon Yun on 2023/10/03.
+//  Copyright © 2023 woin2ee. All rights reserved.
+//
+
+import Foundation
+
+enum UserSettingsSection: Hashable, Sendable {
+
+    case changeLanguage
+
+    case googleDriveSync
+
+    case signOut
+
+}
