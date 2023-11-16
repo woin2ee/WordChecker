ee9aef3ab56eb0b9b760941a72ae452424912514
Jaewon Yun <woin2ee@gmail.com>
2023-11-16 21:26:59 +0900

    Merge assemblies files

--- Sources/iOSCore/DI/Assemblies/ReactorsAssembly.swift
+++ Sources/iOSCore/DI/Assemblies/ReactorsAssembly.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2023 woin2ee. All rights reserved.
 //
 
+import Domain
 import Foundation
 import Swinject
 
@@ -22,6 +23,51 @@ public final class ReactorsAssembly: Assembly {
 
         assemblies.forEach { assembly in
             assembly.assemble(container: container)
+        }
+    }
+
+}
+
+final class WordCheckingReactorAssembly: Assembly {
+
+    func assemble(container: Container) {
+        container.register(WordCheckingReactor.self) { resolver in
+            let wordUseCase: WordRxUseCaseProtocol = resolver.resolve()
+            let userSettingsUseCase: UserSettingsUseCaseProtocol = resolver.resolve()
+
+            return WordCheckingReactor.init(
+                wordUseCase: wordUseCase,
+                userSettingsUseCase: userSettingsUseCase,
+                globalAction: .shared
+            )
+        }
+    }
+
+}
+
+final class WordDetailReactorAssembly: Assembly {
+
+    func assemble(container: Container) {
+        container.register(WordDetailReactor.self) { resolver, uuid in
+            let wordUseCase: WordRxUseCaseProtocol = resolver.resolve()
+
+            return WordDetailReactor.init(
+                uuid: uuid,
+                globalAction: .shared,
+                wordUseCase: wordUseCase
+            )
+        }
+    }
+
+}
+
+final class WordListReactorAssembly: Assembly {
+
+    func assemble(container: Container) {
+        container.register(WordListReactor.self) { resolver in
+            let wordUseCase: WordRxUseCaseProtocol = resolver.resolve()
+
+            return .init(globalAction: .shared, wordUseCase: wordUseCase)
         }
     }
 
--- Sources/iOSCore/DI/Assemblies/RepositoryAssembly.swift
+++ Sources/iOSCore/DI/Assemblies/RepositoryAssembly.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2023 woin2ee. All rights reserved.
 //
 
+import DataDriver
+import Domain
+import ExtendedUserDefaults
 import Foundation
+import RealmSwift
 import Swinject
 
 public final class RepositoryAssembly: Assembly {
@@ -33,6 +37,72 @@ public final class RepositoryAssembly: Assembly {
         assemblies.forEach { assembly in
             assembly.assemble(container: container)
         }
+    }
+
+}
+
+public final class GoogleDriveRepositoryAssembly: Assembly {
+
+    public init() {}
+
+    public func assemble(container: Container) {
+        container.register(GoogleDriveRepositoryProtocol.self) { _ in
+            return GoogleDriveRepository.init(gidSignIn: .sharedInstance)
+        }
+        .inObjectScope(.container)
+    }
+
+}
+
+public final class UnmemorizedWordListStateAssembly: Assembly {
+
+    public init() {}
+
+    public func assemble(container: Container) {
+        container.register(UnmemorizedWordListRepositoryProtocol.self) { _ in
+            return UnmemorizedWordListRepository.init()
+        }
+        .inObjectScope(.container)
+    }
+
+}
+
+open class UserSettingsRepositoryAssembly: Assembly {
+
+    public init() {}
+
+    open func assemble(container: Container) {
+        container.register(UserSettingsRepositoryProtocol.self) { _ in
+            let userDefaults: ExtendedUserDefaults = .standard
+            return UserSettingsRepository.init(userDefaults: userDefaults)
+        }
+        .inObjectScope(.container)
+    }
+
+}
+
+open class WordRepositoryAssembly: Assembly {
+
+    public init() {}
+
+    open func assemble(container: Container) {
+        container.register(WordRepositoryProtocol.self) { _ in
+            var config: Realm.Configuration = makeDefaultRealmConfiguration()
+            guard config.fileURL != nil else {
+                fatalError("Realm's url is nil.")
+            }
+            config.fileURL?.deleteLastPathComponent()
+            config.fileURL?.append(path: "WordCheckerProduct")
+            config.fileURL?.appendPathExtension("realm")
+            guard let realm: Realm = try? .init(configuration: config) else {
+                fatalError("Failed to initialize.")
+            }
+            #if DEBUG
+                print("Realm file url : \(realm.configuration.fileURL?.debugDescription ?? "nil")")
+            #endif
+            return WordRepository.init(realm: realm)
+        }
+        .inObjectScope(.container)
     }
 
 }
