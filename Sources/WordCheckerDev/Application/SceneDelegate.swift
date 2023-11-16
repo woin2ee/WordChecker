c00085e2e8d9005b3aaa1a59b5332373f73ffd86
Jaewon Yun <woin2ee@gmail.com>
2023-11-16 18:27:00 +0900

    Move SceneDelegate to App module

---
+++ Sources/WordCheckerDev/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  WordChecker
+//
+//  Created by Jaewon Yun on 2023/08/23.
+//
+
+import UIKit
+import iOSCore
+
+public class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    public var window: UIWindow?
+
+    public func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = .init(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+
+        let tabBarController: AppTabBarController = .init()
+
+        window?.rootViewController = tabBarController
+    }
+
+}
