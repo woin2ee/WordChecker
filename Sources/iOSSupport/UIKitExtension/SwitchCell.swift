1d047c27c2267507b6a9c9e12de700dd96bea5bb
Jaewon Yun <woin2ee@gmail.com>
2024-01-25 21:12:43 +0900

    Separate SwitchCell from ManualSwitchCell

--- Sources/iOSSupport/UIKitExtension/SwitchCell.swift
+++ Sources/iOSSupport/UIKitExtension/SwitchCell.swift
@@ -1,30 +1,22 @@
-//
-//  ManualSwitchCell.swift
-//  PushNotificationSettings
-//
-//  Created by Jaewon Yun on 11/29/23.
-//  Copyright © 2023 woin2ee. All rights reserved.
-//
-
-import iOSSupport
 import UIKit
 
-final class ManualSwitchCell: RxBaseReusableCell {
+public class SwitchCell: RxBaseReusableCell {
 
-    struct Model {
+    public struct Model {
         let title: String
         let isOn: Bool
+
+        public init(title: String, isOn: Bool) {
+            self.title = title
+            self.isOn = isOn
+        }
     }
 
-    let leadingLabel: UILabel = .init()
-    let trailingSwitch: UISwitch = .init()
-    let wrappingButton: UIButton = .init()
+    public let leadingLabel: UILabel = .init()
+    public let trailingSwitch: UISwitch = .init()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        trailingSwitch.addSubview(wrappingButton)
-        wrappingButton.frame = trailingSwitch.bounds
 
         self.accessoryView = trailingSwitch
         self.selectionStyle = .none
@@ -34,7 +26,7 @@ final class ManualSwitchCell: RxBaseReusableCell {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func bind(model: Model) {
+    public func bind(model: Model) {
         var config: UIListContentConfiguration = .cell()
         config.text = model.title
         self.contentConfiguration = config
