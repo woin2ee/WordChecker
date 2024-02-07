801e1153f6c2881b1ef441c2a1d538e06c2ff9ec
Jaewon Yun <woin2ee@gmail.com>
2024-02-07 13:26:21 +0900

    Add ThemeSetting module

---
+++ Tests/iOSScenesTests/ThemeSettingTests/ThemeSettingTests.swift
@@ -0,0 +1,28 @@
+@testable import ThemeSetting
+
+import XCTest
+
+final class ThemeSettingTests: XCTestCase {
+
+    var sut: ThemeSettingReactor!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+
+    func test_example() {
+        // Given
+
+        // When
+
+        // Then
+        XCTAssertEqual("ThemeSettingKit", "ThemeSettingKit")
+    }
+
+}
