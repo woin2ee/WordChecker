a0572431af81ddc2e214f0c57c41e6b070533746
Jaewon Yun <woin2ee@gmail.com>
2024-02-07 13:54:13 +0900

    Remove dead code

--- Sources/iOSScenes/UserSettings/UserSettingsViewController.swift
+++ Sources/iOSScenes/UserSettings/UserSettingsViewController.swift
@@ -110,10 +110,6 @@ final class UserSettingsViewController: RxBaseViewController, View, UserSettings
     }
 
     override func bindAction() {
-        guard let reactor = self.reactor else {
-            preconditionFailure("After initialization, reactor is not assigned.")
-        }
-
         let itemSelectedEvent = settingsTableView.rx.itemSelected.asSignal()
             .doOnNext { [weak self] in self?.settingsTableView.deselectRow(at: $0, animated: true) }
 
