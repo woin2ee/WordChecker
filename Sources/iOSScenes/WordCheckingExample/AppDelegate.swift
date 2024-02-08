70bf18447bab67665cb18ec115f5c892d4f0042f
Jaewon Yun <woin2ee@gmail.com>
2024-02-08 16:26:25 +0900

    Fix broken example app

--- Sources/iOSScenes/WordCheckingExample/AppDelegate.swift
+++ Sources/iOSScenes/WordCheckingExample/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Jaewon Yun on 2023/08/23.
 //
 
+import iOSSupport
 import UIKit
 import Utility
 
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NetworkMonitor.start()
+        GlobalState.shared.initialize(hapticsIsOn: true, themeStyle: .unspecified)
         return true
     }
 
