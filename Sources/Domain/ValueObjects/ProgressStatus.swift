af5f21d45db38ebb06cbaf2ee838fd2352723079
Jaewon Yun <woin2ee@gmail.com>
2024-02-16 18:03:30 +0900

    Add documentation comments

--- Sources/Domain/ValueObjects/ProgressStatus.swift
+++ Sources/Domain/ValueObjects/ProgressStatus.swift
@@ -8,12 +8,16 @@
 
 import Foundation
 
+/// 어떤 작업의 진행도를 표현할 때 사용하는 값.
 public enum ProgressStatus {
 
+    /// 진행중
     case inProgress
 
+    /// 완료
     case complete
 
+    /// 작업 없음
     case noTask
 
 }
