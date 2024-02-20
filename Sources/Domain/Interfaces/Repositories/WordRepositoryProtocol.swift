a2391f2946809d13e072d976e4a880ae0523f07b
Jaewon Yun <woin2ee@gmail.com>
2024-02-20 16:55:33 +0900

    Make repository methods throw error
    Now, does not ignore errors. Also, improved some logic.

--- Sources/Domain/Interfaces/Repositories/WordRepositoryProtocol.swift
+++ Sources/Domain/Interfaces/Repositories/WordRepositoryProtocol.swift
@@ -11,7 +11,7 @@ public protocol WordRepositoryProtocol {
 
     /// 단어를 저장합니다. 이미 존재하는 UUID 를 가진 단어는 새로 업데이트됩니다.
     /// - Parameter word: 저장할 단어
-    func save(_ word: Word)
+    func save(_ word: Word) throws
 
     func getAllWords() -> [Word]
 
@@ -22,13 +22,13 @@ public protocol WordRepositoryProtocol {
     /// 문자열을 비교할 때 대소문자를 구분하지 않습니다.
     func getWords(by word: String) -> [Word]
 
-    func deleteWord(by uuid: UUID)
+    func deleteWord(by uuid: UUID) throws
 
     func getUnmemorizedList() -> [Word]
 
     func getMemorizedList() -> [Word]
 
     /// 모든 단어를 지우고 새로운 `wordList` 를 저장합니다.
-    func reset(to wordList: [Word])
+    func reset(to wordList: [Word]) throws
 
 }
--- Tests/InfrastructureTests/WordRepositoryTests.swift
+++ Tests/InfrastructureTests/WordRepositoryTests.swift
@@ -16,33 +16,42 @@ import XCTest
 final class WordRepositoryTests: XCTestCase {
 
     var sut: WordRepository!
-    var config: Realm.Configuration!
+    
+    override func setUpWithError() throws {
+        let config: Realm.Configuration = .init(inMemoryIdentifier: String(describing: self))
+        let realm: Realm = try .init(configuration: config)
+        sut = .init(realm: realm)
+    }
     
     override func tearDownWithError() throws {
-        try FileManager().removeItem(at: config.fileURL!)
-        
         sut = nil
-        config = nil
     }
     
     func test_ignoreCaseSensitive_whenGetByWord() throws {
         // Given
-        config = .init()
-        config.fileURL?.deleteLastPathComponent()
-        config.fileURL?.append(path: "\(#function)")
-        config.fileURL?.appendPathExtension(".realm")
-        let realm: Realm = try .init(configuration: config)
-        sut = .init(realm: realm)
-        
         let word1: Domain.Word = try .init(word: "TEST")
         let word2: Domain.Word = try .init(word: "test")
-        sut.save(word1)
-        sut.save(word2)
+        try sut.save(word1)
+        try sut.save(word2)
         
         // When
         let results = sut.getWords(by: "TeST")
         
         // Then
         XCTAssertEqual(results.count, 2)
+    }
+    
+    func test_saveSamePrimaryKeyItem() throws {
+        // Given
+        let testWord: Domain.Word = try .init(word: "TestWord", memorizedState: .memorizing)
+        try sut.save(testWord)
+        
+        testWord.memorizedState = .memorized
+        
+        // When
+        try sut.save(testWord)
+        
+        // Then
+        XCTAssertEqual(sut.getWord(by: testWord.uuid), testWord)
     }
 }
