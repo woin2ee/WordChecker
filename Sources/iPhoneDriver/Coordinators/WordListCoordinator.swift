4d6d49156bf2948c8c387da135068c7813493fbf
Jaewon Yun <woin2ee@gmail.com>
2024-02-20 13:37:14 +0900

    Remove dependency on tab bar item position

--- Sources/iPhoneDriver/Coordinators/WordListCoordinator.swift
+++ Sources/iPhoneDriver/Coordinators/WordListCoordinator.swift
@@ -15,10 +15,16 @@ import WordList
 /// WordList(ViewController) 를 포함하여 연관된 View 들의 화면 전환 책임을 수행하는 객체입니다.
 final class WordListCoordinator: BasicCoordinator {
 
+    var observation: NSKeyValueObservation?
+
     override func start() {
         let viewController: WordListViewControllerProtocol = DIContainer.shared.resolver.resolve()
         viewController.delegate = self
         navigationController.setViewControllers([viewController], animated: false)
+
+        observation = RootTabBarController.shared.observe(to: .doubleTap, tabBarItemAt: 1) { [weak viewController] in
+            viewController?.scrollToTop()
+        }
     }
 
 }
