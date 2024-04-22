18dbccd31a4e65012ccdaf8337abc3663918dd7c
Jaewon Yun <woin2ee@gmail.com>
2024-04-22 16:35:53 +0900

    Fix archive failed error

--- Sources/IOSSupport/Foundations/BaseViewController.swift
+++ Sources/IOSSupport/Foundations/BaseViewController.swift
@@ -7,33 +7,18 @@
 
 import UIKit
 
-public protocol DeallocChecking {
-
-    /// Dealloc checking 활성화 여부
-    ///
-    /// Configuration 이 `DEBUG` 일 때는 기본값이 `true`, `RELEASE` 일 때는 기본값이 `flase` 입니다.
-    var isDeallocCheckEnabled: Bool { get set }
-}
-
-open class BaseViewController: UIViewController, DeallocChecking {
-
-    #if DEBUG
-    public var isDeallocCheckEnabled: Bool = true
-    #elseif RELEASE
-    public var isDeallocCheckEnabled: Bool = false
-    #endif
+open class BaseViewController: UIViewController {
 
     open override func viewDidLoad() {
         super.viewDidLoad()
-
         self.view.backgroundColor = .systemBackground
     }
 
     open override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
 
-        if isDeallocCheckEnabled {
-            self.checkDeallocation()
-        }
+#if DEBUG
+        self.checkDeallocation()
+#endif
     }
 }
