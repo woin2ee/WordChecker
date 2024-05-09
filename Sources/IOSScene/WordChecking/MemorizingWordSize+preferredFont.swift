bcf3758e0512a9c97f37954169b690fbfbd9d7d3
Jaewon Yun <woin2ee@gmail.com>
2024-05-09 21:18:03 +0900

    Add font size settings for iPad

--- Sources/IOSScene/WordChecking/MemorizingWordSize+preferredFont.swift
+++ Sources/IOSScene/WordChecking/MemorizingWordSize+preferredFont.swift
@@ -7,20 +7,37 @@
 //
 
 import Domain_UserSettings
+import IOSSupport
 import UIKit
 
 extension MemorizingWordSize {
     
     var preferredFont: UIFont {
-        switch self {
-        case .small:
-            UIFont.preferredFont(ofSize: 18)
-        case .default:
-            UIFont.preferredFont(ofSize: 21)
-        case .large:
-            UIFont.preferredFont(ofSize: 24)
-        case .veryLarge:
-            UIFont.preferredFont(ofSize: 27)
+        switch UIDevice.current.allowedIdiom {
+
+        case .iPhone:
+            switch self {
+            case .small:
+                UIFont.preferredFont(ofSize: 18)
+            case .default:
+                UIFont.preferredFont(ofSize: 21)
+            case .large:
+                UIFont.preferredFont(ofSize: 24)
+            case .veryLarge:
+                UIFont.preferredFont(ofSize: 27)
+            }
+            
+        case .iPad:
+            switch self {
+            case .small:
+                UIFont.preferredFont(ofSize: 25)
+            case .default:
+                UIFont.preferredFont(ofSize: 28)
+            case .large:
+                UIFont.preferredFont(ofSize: 31)
+            case .veryLarge:
+                UIFont.preferredFont(ofSize: 34)
+            }
         }
     }
 }
